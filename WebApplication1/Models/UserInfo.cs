7fc8324b5908302f316d02118105739b424b55fc
Farshid Mohammadi <fm708801@gmail.com>
2020-05-22 13:06:46 +0430

    990302

---
+++ WebApplication1/Models/UserInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class UserInfo
+    {
+        [Required(ErrorMessage ="وارد کردن نام الزامی است")]
+        [Display(Name ="نام")]
+        public string  FName { get; set; }
+        [Required(ErrorMessage = "وارد کردن نام خانوادگی الزامی است")]
+        [Display(Name = "نام خانوادگی")]
+        public string  LName { get; set; }
+        [Required(ErrorMessage = "وارد کردن سن کاربر الزامی است")]
+        [Display(Name = "سن")]
+        [Range(18,100,ErrorMessage ="حداقل سن 18 سال و بیشترین سن 100 سال می تواند باشد")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "وارد کردن تلفن تماس الزامی است")]
+        [Display(Name = "تلفن")]
+        public int PhoneNumber { get; set; }
+        [Required(ErrorMessage = "وارد کردن ایمیل الزامی است")]
+        [EmailAddress(ErrorMessage ="ایمیل را کامل و بع درستی وارد کنید")]
+        [Display(Name = "ایمیل")]
+        public string UserEmail { get; set; }
+
+    }
+}
---
+++ WebPLAN/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("WebPLAN")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebPLAN")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5292167e-6a56-43f0-8ac4-2ba5e382139b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
